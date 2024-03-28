21333843087f0a4353f178128d78766e88464247
yeryeong <88486391+yeryeong713@users.noreply.github.com>
2024-03-28 12:30:10 +0900

    Create 9_페인팅_기능_만들기.py

---
+++ OpenCV/9_페인팅_기능_만들기.py
@@ -0,0 +1,33 @@
+#9. 페인팅 기능 만들
+import cv2
+import sys
+
+BrushSize = 5
+LColor, RColor = (255,0,0), (0,0,255)
+
+def painting(event, x, y, flags, param):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img,(x,y), BrushSize, LColor, -1)
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        cv2.circle(img,(x,y), BrushSize, RColor, -1)
+    elif event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
+        cv2.circle(img,(x,y), BrushSize, LColor, -1)
+    elif event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_RBUTTON:
+        cv2.circle(img,(x,y), BrushSize, RColor, -1)
+
+    cv2.imshow('Painting', img)
+
+img=cv2.imread('C:/Users/demon/Desktop/img(OpenCV)/apples.jpg')
+
+if img is None:
+  sys.exit('파일을 찾을 수 없음')
+
+cv2.namedWindow('Painting')
+cv2.imshow('Painting',img)
+cv2.setMouseCallback('Painting',painting)
+
+while(True):
+  if cv2.waitKey(1)==ord('q'):
+    cv2.destroyAllWindows()
+    break
+
