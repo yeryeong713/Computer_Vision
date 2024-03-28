d6b69b31d8c488e870a8feabee64ded1b3064d4e
yeryeong <88486391+yeryeong713@users.noreply.github.com>
2024-03-28 12:26:07 +0900

    Create 8_마우스_드래그로_도형_크기_조절하기.py

---
+++ OpenCV/8_마우스_드래그로_도형_크기_조절하기.py
@@ -0,0 +1,28 @@
+#8. 마우스 드래그로 도형 크기 조절하기
+import cv2 as cv
+import sys
+
+img=cv.imread('C:/Users/demon/Desktop/img(OpenCV)/apples.jpg')
+
+if img is None:
+  sys.exit('파일을 찾을 수 없음')
+
+def draw(event, x, y, flags, param):
+    global ix,iy
+    if event==cv.EVENT_LBUTTONDOWN:
+        ix,iy=x,y
+    elif event==cv.EVENT_LBUTTONUP:
+        cv.rectangle(img,(ix,iy),(x,y),(0,0,255),2)
+
+    cv.imshow('Drawing',img)
+
+cv.namedWindow('Drawing')
+cv.imshow('Drawing',img)
+
+cv.setMouseCallback('Drawing',draw)
+
+while(True):
+  if cv.waitKey(1)==ord('q'):
+    cv.destroyAllWindows()
+    break
+
